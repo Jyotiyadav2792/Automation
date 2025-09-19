dc87f5f6813915398362f5b8806f7f953dfe5965
Jyoti yadav <jyotiyadav2792@gmail.com>
2025-09-19 17:38:03 +0530

    Java Program

---
+++ src/Ex01_JavaBasics/Comment.java
@@ -0,0 +1,21 @@
+package Ex01_JavaBasics;
+
+public class Comment {
+    public static void main(String[] args) {
+        /*System.out.println("Hello World");
+        System.out.println("Hello 1"");
+        * If I do ctrl shift / then comment will enable
+        *   what is your name
+
+
+        */
+    }
+}// single comment//
+
+
+/* multi comment
+* Name:
+* Class:
+* School:
+*
+ */
---
+++ src/Ex01_JavaBasics/NoMethod.java
@@ -0,0 +1,7 @@
+package Ex01_JavaBasics;
+
+public class NoMethod {
+
+}
+//we can create a program without Main method but not able to run it as
+// JVM always search for a main method to run a program//
